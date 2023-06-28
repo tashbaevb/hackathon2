a653743156e0ef3922396cbece4548a075db6612
Baitur <tawbaev777@gmail.com>
2023-06-29 02:27:18 +0600

    hack
    hack

--- API/models.py
+++ API/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-#from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 INSTITUTE_CATEGORY = (('Детский сад', 'Детский сад'),
                       ('Школа', 'Школа'),
